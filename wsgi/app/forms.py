76d9bdcbd9561daea46b129be69a24da67548e6b
ec16544 <i.s.shalev@se16.qmul.ac.uk>
2017-03-21 11:49:31 +0000

    Added folders

---
+++ wsgi/app/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import TextField, validators
+
+class MessageForm(FlaskForm):
+   message = TextField(u'What is on your mind?', [validators.optional(), validators.length(max=200)])
+
+
+
